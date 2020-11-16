8de9160e4a9901cd011a25e9d36b0f042fdcdc4a
reinhart <reinhartandreas96@gmail.com>
2020-11-16 20:18:16 +0700

    set logo image, change border menu, set pt pb z-index background position fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,29 +20,29 @@ const Header = () => {
   `)
 
   return (
-    <header className="header container pt-6 pb-12 md:pt-12">
+    <header className="header container pt-6 pb-12 md:pt-3 md:pb-3 fixed top-0 z-10 background-black-ds shadow-md">
       <nav className="flex justify-between items-center">
         {/* Logo */}
         <Link to="/">
-          <img alt="Logo" className="w-24 md:w-32" src="logo.svg" />
+          <img alt="Logo" className="w-24 md:w-32" src="streetcrownLogo.png" />
         </Link>
 
         {/* Hamburger menu */}
         <button
-          className="sm:hidden"
+          className="sm:hidden text-white"
           onClick={() => setIsMenuOpen(true)}
           aria-label="Open Menu"
         >
-          <FaBars className="h-6 w-auto text-gray-900 fill-current -mt-1" />
+          <FaBars className="h-6 w-auto text-white fill-current -mt-1" />
         </button>
         
         {/* Nav menu on tab and pc resolution */}
-        <div className="hidden sm:block">
+        <div className="hidden sm:block text-white">
           {site.data.menu.map((link, key) => (
             <Link
               key={`menu_desktop_link${key}`}
-              className="ml-6 sm:ml-8 text-sm sm:text-base font-medium px-px border-b-2 pb-2 border-transparent text-gray-700 hover:text-gray-800 hover:border-gray-200 transition duration-150 ease-in-out"
-              activeClassName="border-blue-600 text-gray-900 hover:border-blue-600"
+              className="ml-6 sm:ml-8 text-sm sm:text-base font-medium px-px border-b-2 pb-2 border-transparent hover:border-red-200 transition duration-150 ease-in-out text-white"
+              activeClassName="border-red-600 hover:border-red-600"
               to={link.to}
             >
               {link.name}
