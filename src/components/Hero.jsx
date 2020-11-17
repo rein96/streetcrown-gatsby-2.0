6d3c31b6259b098be1998f2f7e4efc6e63ddf92a
reinhart <reinhartandreas96@gmail.com>
2020-11-17 22:35:27 +0700

    hero h1 h2 text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,13 @@ import './Hero.scss'
 
 const Hero = () => (
   <section className="hero-container flex-center-center">
-    <section className='hero-content absolute text-center'>
-      <h1 className='text-2xl text-white'>StreetCrown | Professional Auto Detailer</h1>
-      <h2 className='text-6xl text-white'>Shiny inside Shiny outside </h2>
+    <section className='hero-content absolute text-center font-semibold text-white'>
+      <h1 className='text-2xl'>
+        <span className='color-red-ds'>StreetCrown</span> | Professional Auto Detailer
+      </h1>
+      <h2 className='text-6xl'>
+        <span className='color-red-ds'>Shiny</span> inside <span className='color-red-ds'>Shiny</span> outside
+      </h2>
     </section>
     {/* <button className=''>Book Now</button> */}
     <img alt="Logo" className="hero-image hero-portrait" src="hero-portrait.jpg" />
