19dc7fdec4bf6c2028a86afb2b8cece3d1255802
reinhart <reinhartandreas96@gmail.com>
2020-11-21 18:48:35 +0700

    create element for streetcrown-slogan-large and streetcrown-slogan-mobile add media queries in hero.s.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,19 @@ import './Hero.scss'
 const Hero = () => (
   <section className="hero-container relative flex-center-center">
     <section className='hero-content absolute text-center font-semibold text-white'>
-      <h1 className='text-2xl'>
+      <h1 className='streetcrown-title-h1 text-xl lg:text-2xl'>
         <span className='color-red-ds'>StreetCrown</span> | Professional Auto Detailer
       </h1>
-      <h2 className='text-6xl'>
-        <span className='color-red-ds'>Shiny</span> inside <span className='color-red-ds'>Shiny</span> outside
+      <h2 className='streetcrown-slogan-large text-5xl lg:text-6xl'>
+        <span className='color-red-ds'>Shiny</span> <span>inside</span> <span className='color-red-ds'>Shiny</span> <span>outside</span>
+      </h2>
+      <h2 className='streetcrown-slogan-mobile'>
+        <div className='text-left'>
+          <span className='color-red-ds'>Shiny</span> <span>inside</span>
+        </div>
+        <div className='text-right'>
+          <span className='color-red-ds'>Shiny</span> <span>outside</span>
+        </div>
       </h2>
       <button className='absolute btnStyle btn-float btn-md background-red-ds mt-8'>Book Now</button>
     </section>
